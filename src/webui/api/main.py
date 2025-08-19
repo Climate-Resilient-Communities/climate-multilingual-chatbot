5694650de5e598f27479df48258cedadb6cd0865
Jordan Lenard David <jordan_david02@hotmail.com>
2025-08-18 22:59:51 -0400

    [u] changed port from hardcoded

--- src/webui/api/main.py
+++ src/webui/api/main.py
@@ -383,10 +383,11 @@ async def global_exception_handler(request: Request, exc: Exception):
     )
 
 if __name__ == "__main__":
+    port = int(os.getenv("PORT", 8000))
     uvicorn.run(
         "main:app",
         host="0.0.0.0",
-        port=8000,
+        port=port,
         reload=False,
         log_level="info"
     )
